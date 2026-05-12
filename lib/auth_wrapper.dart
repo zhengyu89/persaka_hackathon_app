5b2ee4b1707bf9e38118ecb903851a4d7c6dc8f7
Reem Omer <omeribrahim@graduate.utm.my>
2026-05-12 23:28:04 +0300

    PM-60: redesign profile screen to match app theme

--- lib/auth_wrapper.dart
+++ lib/auth_wrapper.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'features/auth/email_verification/screens/email_verification_screen.dart';
 import 'features/auth/password_auth/screens/login_screen.dart';
-//import 'features/home/screens/home_screen.dart';
+import 'features/home/screens/home_screen.dart';
 import 'features/main/screens/main_screen.dart';
 
 class AuthWrapper extends StatelessWidget {
